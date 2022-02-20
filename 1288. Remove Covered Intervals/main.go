a086f6ff80d488109eed2edcc0fb59e93d323f13
methaboat <metha.boat@gmail.com>
2022-02-21 01:30:05 +0700

    test

---
+++ 1288. Remove Covered Intervals/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	// a := [[1,4],[3,6],[2,8]]
+	// fmt.Print( removeCoveredIntervals(a)
+}
+
+func removeCoveredIntervals(intervals [][]int) int {
+	// result := [][]int{}
+	// for i, j := range intervals {
+	// 	// if result == nil {
+	// 	// 	result[0][0] = i
+	// 	// 	result[0][1] = j
+	// 	// }
+	// 	fmt.Println(i, j)
+	// }
+
+	return 0
+}
